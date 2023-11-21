1bd5a626a44d15101ec0f9f3a622081b7abe154e
St0rmie <duartebritosousa@hotmail.com>
2023-11-21 02:15:27 +0000

    PROJ: renamed files

---
+++ src/client/client_main.cpp
@@ -0,0 +1,33 @@
+#include "client_main.hpp"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "commands.hpp"
+
+int main(int argc, char *argv[]) {
+	CommandManager commandManager;
+	registerCommands(commandManager);
+
+	while (!std::cin.eof()) {
+		commandManager.waitCommand();
+	}
+
+	std::cout << "Bye Bye!" << std::endl;
+}
+
+void registerCommands(CommandManager &manager) {
+	manager.registerCommand(std::make_shared<LoginCommand>());
+	manager.registerCommand(std::make_shared<CreateAuctionCommand>());
+	manager.registerCommand(std::make_shared<CloseAuctionCommand>());
+	manager.registerCommand(std::make_shared<ListStartedAuctionsCommand>());
+	manager.registerCommand(std::make_shared<ListBiddedAuctionsCommand>());
+	manager.registerCommand(std::make_shared<ListAllAuctionsCommand>());
+	manager.registerCommand(std::make_shared<ShowAssetCommand>());
+	manager.registerCommand(std::make_shared<BidCommand>());
+	manager.registerCommand(std::make_shared<ShowRecordCommand>());
+	manager.registerCommand(std::make_shared<LogoutCommand>());
+	manager.registerCommand(std::make_shared<UnregisterCommand>());
+	manager.registerCommand(std::make_shared<ExitCommand>());
+}
---
+++ src/client/client_main.hpp
@@ -0,0 +1,8 @@
+#ifndef __CLIENT_MAIN__
+#define __CLIENT_MAIN__
+
+#include "commands.hpp"
+
+void registerCommands(CommandManager &manager);
+
+#endif
