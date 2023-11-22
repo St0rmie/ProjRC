f21da5f6af24644041cb245d8d702fa54c715cc3
St0rmie <duartebritosousa@hotmail.com>
2023-11-22 23:18:14 +0000

    PROJ: progress on client socket creation and hostname resolve

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,5 +1,55 @@
 #include "client.hpp"
 
+Client::Client(std::string &hostname, std::string &port) {
+	// Create a UDP socket
+	if ((this->_udp_socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		throw SocketException();
+	}
+	this->resolveServerAddress(hostname, port);
+}
+
+Client::~Client() {
+	if (this->_udp_socket_fd != -1) {
+		close(this->_udp_socket_fd);
+	}
+	if (this->_tcp_socket_fd != -1) {
+		close(this->_tcp_socket_fd);
+	}
+	if (this->_server_udp_addr != NULL) {
+		freeaddrinfo(this->_server_udp_addr);
+	}
+	if (this->_server_tcp_addr != NULL) {
+		freeaddrinfo(this->_server_tcp_addr);
+	}
+}
+
+void Client::resolveServerAddress(std::string &hostname, std::string &port) {
+	struct addrinfo hints;
+	int errcode;
+
+	// Convert string to C string (char*)
+	const char *host = hostname.c_str();
+	const char *port_str = port.c_str();
+
+	// Get UDP address
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;       // IPv4
+	hints.ai_socktype = SOCK_DGRAM;  // UDP socket
+	errcode = getaddrinfo(host, port_str, &hints, &this->_server_udp_addr);
+	if (errcode != 0) {
+		throw ResolveHostnameException();
+	}
+
+	// Get TCP address
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;        // IPv4
+	hints.ai_socktype = SOCK_STREAM;  // TCP socket
+	errcode = getaddrinfo(host, port_str, &hints, &this->_server_tcp_addr);
+	if (errcode != 0) {
+		throw ResolveHostnameException();
+	}
+}
+
 void Client::login(int user_id) {
 	this->_user_id = user_id;
 }
@@ -10,4 +60,8 @@ void Client::logout() {
 
 int Client::isLoggedIn() {
 	return _user_id > -1;
+}
+
+int Client::getLoggedInUser() {
+	return _user_id;
 }
--- src/client/client.hpp
+++ src/client/client.hpp
@@ -1,14 +1,53 @@
 #ifndef __CLIENT__
 #define __CLIENT__
 
+#include <netdb.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <string>
+
+#include "shared/protocol.hpp"
+
 #define LOGGED_OUT -1
 
-class Client {
+// Thrown when there is an error when trying to create a socket
+class SocketException : public std::runtime_error {
    public:
+	SocketException()
+		: std::runtime_error("[Error] Failed to create socket.") {}
+};
+
+// Thrown when the hostname couldn't be resolved
+class ResolveHostnameException : public std::runtime_error {
+   public:
+	ResolveHostnameException()
+		: std::runtime_error("[Error] Failed to resolve hostname.") {}
+};
+
+class Client {
 	int _user_id = LOGGED_OUT;
+
+	int _udp_socket_fd = -1;
+	int _tcp_socket_fd = -1;
+	struct addrinfo* _server_udp_addr = NULL;
+	struct addrinfo* _server_tcp_addr = NULL;
+
+	void resolveServerAddress(std::string& hostname, std::string& port);
+	void sendUdpMessage(ProtocolMessage& message);
+	void waitForUdpMessage(ProtocolMessage& message);
+	void openTcpSocket();
+	void sendTcpMessage(ProtocolMessage& message);
+	void waitForTcpMessage(ProtocolMessage& message);
+	void closeTcpSocket();
+
+   public:
+	Client(std::string& hostname, std::string& port);
+	~Client();
 	void login(int user_id);
 	void logout();
 	int isLoggedIn();
+	int getLoggedInUser();
 };
 
 #endif
