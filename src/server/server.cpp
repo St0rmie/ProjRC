861326cc2c0e9f2c755fde5784e68653568421af
St0rmie <duartebritosousa@hotmail.com>
2023-12-09 21:36:55 +0000

    PROJ: database

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -57,10 +57,58 @@ void wait_for_udp_message(Server &server, RequestManager &manager) {
 
 void processTCP(Server &server, RequestManager &manager) {}
 
+void RequestManager::registerRequest(std::shared_ptr<RequestHandler> handler,
+                                     int type) {
+	if (type == UDP_MESSAGE) {
+		this->_udp_handlers.insert({handler->_protocol_code, handler});
+	} else {
+		this->_tcp_handlers.insert({handler->_protocol_code, handler});
+	}
+}
+
+void RequestManager::registerRequestHandlers(RequestManager &manager) {
+	manager.registerRequest(std::make_shared<LoginRequest>(), UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<LogoutRequest>(), UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<UnregisterRequest>(), UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<ListAllAuctionsRequest>(),
+	                        UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<ListBiddedAuctionsRequest>(),
+	                        UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<ListStartedAuctionsRequest>(),
+	                        UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<ShowRecordRequest>(), UDP_MESSAGE);
+	manager.registerRequest(std::make_shared<OpenAuctionRequest>(),
+	                        TCP_MESSAGE);
+	manager.registerRequest(std::make_shared<CloseAuctionRequest>(),
+	                        TCP_MESSAGE);
+	manager.registerRequest(std::make_shared<ShowAssetRequest>(), TCP_MESSAGE);
+	manager.registerRequest(std::make_shared<BidRequest>(), TCP_MESSAGE);
+}
+
+void RequestManager::callHandlerRequest(MessageAdapter &message, Server &server,
+                                        Address &address, int type) {
+	std::string rec_proto_code = message.getn(PROTOCOL_SIZE);
+	if (type == UDP_MESSAGE) {
+		auto handler = this->_udp_handlers.find(rec_proto_code);
+		if (handler == this->_udp_handlers.end()) {
+			throw UnknownHandlerException();
+			return;
+		}
+		handler->second->handle(message, server, address);
+	} else {
+		auto handler = this->_tcp_handlers.find(rec_proto_code);
+		if (handler == this->_tcp_handlers.end()) {
+			throw UnknownHandlerException();
+			return;
+		}
+		handler->second->handle(message, server, address);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	Server server(argc, argv);
 	RequestManager requestManager;
-	registerRequestHandlers(requestManager);
+	requestManager.registerRequestHandlers();
 
 	pid_t c_pid = fork();
 	if (c_pid == 0) {
